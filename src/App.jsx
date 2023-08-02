cac79bcfb1727e12201ec81f333b57451b031b34
Himel <shariyerhimel541@gmail.com>
2023-08-03 02:17:30 +0600

    added products

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './component/Header/Header'
+import Shop from './component/Shop/Shop'
+import Product from './component/Product/Product'
 
 function App() {
   return (
     <div className=''>
       <Header></Header>
+      <Shop></Shop>
+
     </div>
   )
 }
